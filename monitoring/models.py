2382cf94101eb9a0b65979ca453958c34ae2f006
Maribel Fonseca <maribel@guegue.net>
2019-07-29 13:57:55 +0000

    link contract country to country model

--- monitoring/models.py
+++ monitoring/models.py
@@ -1,9 +1,3 @@
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
 
@@ -40,7 +34,7 @@ class Contact(models.Model):
     community = models.CharField(max_length=40, blank=True, null=True)
     municipality = models.CharField(max_length=40, blank=True, null=True)
     city = models.CharField(max_length=40, blank=True, null=True)
-    country = models.CharField(max_length=2, blank=True, null=True)
+    country = models.ForeignKey('Country', on_delete=models.SET_NULL, null=True)
     education_id = models.IntegerField(blank=True, null=True)
     phone_personal = models.CharField(max_length=20, blank=True, null=True)
     phone_work = models.CharField(max_length=20, blank=True, null=True)
@@ -56,26 +50,24 @@ class Contact(models.Model):
 
     class Meta:
         ordering = ['name']
-        managed = False
         db_table = 'contact'
 
 
-class CountryAux(models.Model):
-    pais_en_espaniol = models.CharField(max_length=255)
-    pais_en_ingles = models.CharField(max_length=255)
+class Country(models.Model):
+    name_es = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     codigo_numerico = models.IntegerField()
-    alfa2 = models.CharField(primary_key=True, max_length=2)
+    id = models.CharField(primary_key=True, max_length=2)
     alfa3 = models.CharField(max_length=3)
-    coordenada_x = models.CharField(max_length=255)
-    coordenada_y = models.CharField(max_length=255)
+    x = models.CharField(max_length=255)
+    y = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.pais_en_ingles
+        return self.name
 
     class Meta:
-        ordering = ['pais_en_ingles']
-        managed = False
-        db_table = 'country_aux'
+        ordering = ['name']
+        db_table = 'country'
 
 
 class DataList(models.Model):
