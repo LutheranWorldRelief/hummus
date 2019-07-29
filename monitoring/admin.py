60c54673e06c579de70a032c7bc9710d191e70fd
Maribel Fonseca <maribel@guegue.net>
2019-07-29 13:34:52 +0000

    admin list

--- monitoring/admin.py
+++ monitoring/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
 from django.apps import apps
 
-models = apps.get_models()
 
+# based on https://hackernoon.com/automatically-register-all-models-in-django-admin-django-tips-481382cf75e5
+
+class ListAdminMixin(object):
+    def __init__(self, model, admin_site):
+        not_allowed = ['password', 'colors']
+        self.list_display = [field.name for field in model._meta.fields if field.name not in not_allowed ]
+        if 'name' in self.list_display:
+            self.search_fields = ['name']
+        super(ListAdminMixin, self).__init__(model, admin_site)
+
+
+models = apps.get_models()
 for model in models:
+    admin_class = type('AdminClass', (ListAdminMixin, admin.ModelAdmin), {})
     try:
-        admin.site.register(model)
+        admin.site.register(model, admin_class)
     except admin.sites.AlreadyRegistered:
         pass
