45c6514e560cb40e9daadcd9b428c6ad48f46cda
Maribel Fonseca <maribel@guegue.net>
2019-08-12 20:27:40 +0000

    adding static and graphics

--- monitoring/graphic.py
+++ monitoring/graphic.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.db.models.functions import Concat
+from django.db.models import Sum, Count, Q, Value, CharField
+from django.db import connection
 
 from .models import *
 
@@ -23,7 +26,16 @@ def cantidadEventos(request):
 
 @csrf_exempt
 def graficoActividades(request):
-    return JsonResponse({'foo':'bar'})
+    result = Attendance.objects.order_by('event__structure_id', 'event__structure__description', 'event__structure__project__name').values(
+        'event__structure_id', 'event__structure__description', 'event__structure__project__name').annotate(
+        m=Count('id', filter=Q(sex='M')),
+        f=Count('id', filter=Q(sex='M')),
+        total=Count('id'))
+    for row in result:
+        row['name'] = str(row['event__structure__description']) + ' / ' + str(row['event__structure__project__name'])
+        row['activity_id'] = row['event__structure_id']
+    data = {'actividades': list(result) }
+    return JsonResponse(data)
 
 @csrf_exempt
 def paises(request):
@@ -33,8 +45,10 @@ def paises(request):
 
 @csrf_exempt
 def rubros(request):
-    rubros = Product.objects.count()
-    data = {'rubros': rubros }
+    result = Product.objects.all().values()
+    for row in result:
+        row['rubro'] = row['name_es']
+    data = {'rubros': list(result) }
     return JsonResponse(data)
 
 @csrf_exempt
@@ -63,13 +77,37 @@ def proyectosMetas(request):
 def graficoAnioFiscal(request):
     return JsonResponse({'foo':'bar'})
 
+def dictfetchall(cursor):
+    "Return all rows from a cursor as a dict"
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
 @csrf_exempt
 def graficoEdad(request):
-    return JsonResponse({'foo':'bar'})
+    cursor = connection.cursor()
+    cursor.execute("SELECT filter.name AS type, \
+                        COUNT(case when sex = 'F' then 1 else NULL end) AS f, \
+                        COUNT(case when sex = 'M' then 1 else NULL end) AS m, \
+                        COUNT(sex) as total \
+                        FROM contact \
+			JOIN filter ON filter.slug='age' AND date_part('YEAR', age(birthdate)) \
+			BETWEEN CAST( filter.start as INTEGER) AND CAST( filter.end as INTEGER) \
+                        GROUP BY filter.name ")
+    result = dictfetchall(cursor)
+    return JsonResponse({'edad': result})
 
 @csrf_exempt
 def graficoEducacion(request):
-    return JsonResponse({'foo':'bar'})
+    result = Contact.objects.order_by('education__name').values('education__name').annotate(
+        m=Count('id', filter=Q(sex='M')),
+        f=Count('id', filter=Q(sex='M')),
+        total=Count('id'))
+    for row in result:
+        row['type'] = str(row['education__name'])
+    data = {'educacion': list(result) }
+    return JsonResponse(data)
 
 @csrf_exempt
 def graficoEventos(request):
