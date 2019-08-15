cc2e31bc4c383f34061c715e48ab1581a8e871db
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2019-08-15 10:27:01 -0600

    modificando interfaz de lista de contacto,agregando list_display,list_filter,ordering y cantidad de registros por pagina

--- monitoring/admin.py
+++ monitoring/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.apps import apps
 from import_export.admin import ImportExportModelAdmin
+from .models import Contact
 
 
 # based on https://hackernoon.com/automatically-register-all-models-in-django-admin-django-tips-481382cf75e5
@@ -9,7 +10,7 @@ class ListAdminMixin(object):
     def __init__(self, model, admin_site):
         not_allowed = ['password', 'colors']
         self.list_filter = []
-        self.list_display = [field.name for field in model._meta.fields if field.name not in not_allowed ]
+        self.list_display = [field.name for field in model._meta.fields if field.name not in not_allowed]
         if 'name' in self.list_display:
             self.search_fields = ['name']
         if 'type' in self.list_display:
@@ -18,6 +19,21 @@ class ListAdminMixin(object):
             self.list_filter.append(('country', admin.RelatedOnlyFieldListFilter))
         super(ListAdminMixin, self).__init__(model, admin_site)
 
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'document', 'country', 'organization', 'type', 'title')
+    list_display_links = ['name', 'country', 'type']
+    list_per_page = 20
+    list_max_show_all = 50
+    ordering = ['name']
+    list_filter = [
+        ('country', admin.RelatedOnlyFieldListFilter),
+        'type'
+    ]
+    search_fields = ['name']
+
+
+admin.site.register(Contact, ContactAdmin)
 
 models = apps.get_models()
 for model in models:
