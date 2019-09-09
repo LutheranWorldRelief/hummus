05e11d5f9536976ea4867a16af200440ed070d01
Javier Wilson <javier.wilson@gmail.com>
2019-09-09 19:51:13 +0000

    using first

--- monitoring/signals.py
+++ monitoring/signals.py
@@ -7,9 +7,9 @@ from django.conf import settings
 
 @receiver(user_logged_in)
 def on_login(sender, request, **kwargs):
-    languageUser = Profile.objects.filter(user_id=request.user.id).values('language')
+    languageUser = Profile.objects.filter(user_id=request.user.id).values('language').first()
     if languageUser:
-        translation.activate(languageUser[0]['language'])
-        request.session[translation.LANGUAGE_SESSION_KEY] = languageUser[0]['language']
+        translation.activate(languageUser['language'])
+        request.session[translation.LANGUAGE_SESSION_KEY] = languageUser['language']
         response = HttpResponse()
-        response.set_cookie(settings.LANGUAGE_COOKIE_NAME, languageUser[0]['language'])
+        response.set_cookie(settings.LANGUAGE_COOKIE_NAME, languageUser['language'])
