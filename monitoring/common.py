281d0f34c6d38d7a8f02dee0dea60e97b2d9c434
Javier Wilson <javier.wilson@gmail.com>
2019-09-30 16:09:06 +0000

    requires domain or superuser or permission to view projects

--- monitoring/common.py
+++ monitoring/common.py
@@ -1,16 +1,65 @@
 import re
 
 from django.conf import settings
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.db.models import Func, Value
 from django.http import JsonResponse
 from django.utils import translation
 from django.utils.translation import gettext_lazy as _
 
 
+def domain_required(login_url=None, redirect_field_name=REDIRECT_FIELD_NAME):
+
+    def decorator(view_func):
+        @wraps(view_func)
+        def _wrapped_view(request, *args, **kwargs):
+            # domain required
+            domain = request.user.email.endswith('{}%s'.format(MICROSOFT_DOMAIN) )
+
+            # super user can always get int
+            superuser = request.user.is_superuser
+
+            # if you can view projects, you can view this...
+            permission = request.user.has_perm('monitoring.project.can_view')
+
+            if domain or superuser or permission:
+                return view_func(request, *args, **kwargs)
+            path = request.build_absolute_uri()
+            resolved_login_url = resolve_url(login_url or settings.LOGIN_URL)
+            # If the login url is the same scheme and net location then just
+            # use the path as the "next" url.
+            login_scheme, login_netloc = urlparse(resolved_login_url)[:2]
+            current_scheme, current_netloc = urlparse(path)[:2]
+            if ((not login_scheme or login_scheme == current_scheme) and
+                    (not login_netloc or login_netloc == current_netloc)):
+                path = request.get_full_path()
+            from django.contrib.auth.views import redirect_to_login
+            return redirect_to_login(
+                path, resolved_login_url, redirect_field_name)
+        return _wrapped_view
+    return decorator
+
+
+class DomainRequiredMixin(UserPassesTestMixin):
+    def test_func(self):
+        # domain required
+        domain = self.request.user.email.endswith('{}%s'.format(MICROSOFT_DOMAIN) )
+
+        # super user can always get int
+        superuser = self.request.user.is_superuser
+
+        # if you can view projects, you can view this...
+        permission = request.user.has_perm('monitoring.project.can_view')
+
+        return domain or superuser or permission
+
+
 def language_no_region(language):
     if '-' in language:
         language, _, region = language.lower().partition('-')
     return language
+
 
 def get_localized_name(column):
     language = language_no_region(translation.get_language())
