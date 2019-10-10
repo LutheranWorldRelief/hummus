57d8a5a7b398458a7902e6162720d6301863097c
Javier Wilson <javier.wilson@gmail.com>
2019-10-10 04:07:48 +0000

    pylint admin

--- monitoring/admin.py
+++ monitoring/admin.py
@@ -1,17 +1,20 @@
+"""
+admin customization for 'monitoring'
+"""
 from django.apps import apps
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 from django.utils.html import format_html
 
 from jet.admin import CompactInline
-from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
-from .models import *
+from .models import (Contact, Project, Organization, ProjectContact, Profile, SubProject,
+                     LWRRegion, Country)
 
 
 # Change default query
-class AdminForUserMixin(object):
+class AdminForUserMixin:
 
     def save_model(self, request, obj, form, change):
         if obj.id and hasattr(obj, 'created_user'):
@@ -23,17 +26,18 @@ class AdminForUserMixin(object):
     def get_queryset(self, request):
         if request.user:
             return self.model.objects.for_user(request.user)
-        else:
-            return super().get_queryset(request)
+        return super().get_queryset(request)
 
 
-# based on https://hackernoon.com/automatically-register-all-models-in-django-admin-django-tips-481382cf75e5
+# based on
+# https://hackernoon.com/automatically-register-all-models-in-django-admin-django-tips-481382cf75e5
 
-class ListAdminMixin(object):
+class ListAdminMixin:
     def __init__(self, model, admin_site):
         not_allowed = ['password', 'colors']
         self.list_filter = []
-        self.list_display = [field.name for field in model._meta.fields if field.name not in not_allowed]
+        self.list_display = [field.name for field in model._meta.fields
+                             if field.name not in not_allowed]
         if 'name' in self.list_display:
             self.search_fields = ['name']
         if 'type' in self.list_display:
@@ -52,7 +56,7 @@ class ProjectContactInline(admin.TabularInline):
     can_delete = False
     extra = 0
 
-    def has_add_permission(self, request, **kwargs):
+    def has_add_permission(self, request, obj=None):
         return False
 
 
@@ -92,8 +96,8 @@ class ProjectAdmin(AdminForUserMixin, admin.ModelAdmin):
         SubProjectsInline,
     ]
     list_display = (
-        'code', 'name', 'status', 'get_countries', 'lwrregion', 'targetmen', 'targetwomen', 'get_women', 'get_men',
-        'get_total')
+        'code', 'name', 'status', 'get_countries', 'lwrregion', 'targetmen', 'targetwomen',
+        'get_women', 'get_men', 'get_total')
     list_per_page = 20
     list_max_show_all = 50
     list_display_links = ['name']
@@ -103,7 +107,8 @@ class ProjectAdmin(AdminForUserMixin, admin.ModelAdmin):
     readonly_fields = ['created', 'updated', 'show_salesforce_url']
     fieldsets = [
         (_('General information'),
-         {'fields': ['code', 'name', 'status', 'logo', 'colors', 'url', 'lwrregion', 'show_salesforce_url', 'created', 'updated',]}),
+         {'fields': ['code', 'name', 'status', 'logo', 'colors', 'url', 'lwrregion',
+                     'show_salesforce_url', 'created', 'updated',]}),
         (_('Countries'), {'fields': ['countries']}),
         (_('Date information'), {'fields': ['start', 'end']}),
         (_('Goal'), {'fields': ['targetmen', 'targetwomen']}),
@@ -125,25 +130,26 @@ class ProjectAdmin(AdminForUserMixin, admin.ModelAdmin):
 
     get_countries.short_description = _('Countries')
 
-    def get_women(self, obj):
+    def get_women(self):
         return 0
 
     get_women.short_description = _('Women')
 
-    def get_men(self, obj):
+    def get_men(self):
         return 0
 
     get_men.short_description = _('Men')
 
-    def get_total(self, obj):
-        return self.get_men(obj) + self.get_women(obj)
+    def get_total(self):
+        return self.get_men() + self.get_women()
 
     get_total.short_description = _('T')
 
 
 class OrganizationAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'name', 'description', 'country', 'organization_parent', 'organization_type', 'is_implementer')
+        'id', 'name', 'description', 'country', 'organization_parent', 'organization_type',
+        'is_implementer')
     list_per_page = 20
     list_max_show_all = 50
     ordering = ['id']
@@ -156,17 +162,16 @@ class OrganizationAdmin(admin.ModelAdmin):
     search_fields = ['id', 'name', 'description', 'country__name']
     exclude = ['id']
 
-    def organization_parent(self, object):
-        if object.organization_id is not None:
-            return object.name
-        else:
-            return ''
+    def organization_parent(self, obj):
+        if obj.organization_id is not None:
+            return obj.name
+        return ''
 
 
 class ProjectContactAdmin(AdminForUserMixin, admin.ModelAdmin):
     list_display = (
-        'id', 'project', 'contact', 'getCountryContact', 'product', 'area', 'date_entry_project', 'date_end_project',
-        'yield_field')
+        'id', 'project', 'contact', 'get_country_contact', 'product', 'area', 'date_entry_project',
+        'date_end_project', 'yield_field')
     list_display_links = ['id', 'project']
     ordering = ['-date_entry_project']
     readonly_fields = ['created', 'updated', ]
@@ -179,10 +184,10 @@ class ProjectContactAdmin(AdminForUserMixin, admin.ModelAdmin):
         ('contact__country', admin.RelatedOnlyFieldListFilter)
     ]
 
-    def getCountryContact(self, obj):
+    def get_country_contact(self, obj):
         return obj.contact.country
 
-    getCountryContact.short_description = 'Country'
+    get_country_contact.short_description = 'Country'
 
 
 class ProfileAdmin(admin.ModelAdmin):
@@ -207,7 +212,8 @@ class SubprojectAdmin(AdminForUserMixin, admin.ModelAdmin):
     search_fields = ['name', 'code', 'project__name', 'status']
     fieldsets = [
         (_('General information'),
-         {'fields': ['code', 'name', 'project', 'status', 'organization', 'country', 'show_salesforce_url', 'created', 'updated', ]}),
+         {'fields': ['code', 'name', 'project', 'status', 'organization', 'country',
+                     'show_salesforce_url', 'created', 'updated', ]}),
         (_('Date information'), {'fields': ['start', 'end',]}),
         (_('Goals'), {'fields': ['targetimen', 'targetiwomen', 'targetmen', 'targetwomen']}),
     ]
@@ -250,8 +256,8 @@ admin.site.register(SubProject, SubprojectAdmin)
 admin.site.register(LWRRegion, LWRRegionAdmin)
 admin.site.register(Country, CountryAdmin)
 
-models = apps.get_models()
-for model in models:
+MODELS = apps.get_models()
+for model in MODELS:
     admin_class = type('AdminClass', (ListAdminMixin, ImportExportModelAdmin), {})
     try:
         admin.site.register(model, admin_class)
