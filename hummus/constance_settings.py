a0963059cb5e8ebe53be55fcbfa8499999e59660
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2019-10-14 11:40:25 -0600

    declarando nueva constante HEADER_ROW

--- hummus/constance_settings.py
+++ hummus/constance_settings.py
@@ -4,5 +4,6 @@ settings for django constance
 CONSTANCE_CONFIG = {
     'SALESFORCE_SYNC': (True, 'Salesforce Syncronization', bool),
     'SALESFORCE_FREQ': (60, 'Salesforce Syncronization Frequency in minutes', int),
-    'START_ROW': (2, 'Data starts at this row', int),
+    'HEADER_ROW': (2, 'Row', int),
+    'START_ROW': (3, 'Data starts at this row', int),
 }
