eb617697d20aa88ec464aeee45849b2a1c681019
Javier Wilson <javier.wilson@gmail.com>
2019-10-16 17:19:26 +0000

    at least pylint is happy

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -50,8 +50,9 @@ urlpatterns = [
          name='grafico-pais-eventos'),
 
     # import
-    path('import/participants/step1', TemplateView.as_view(template_name='import/step1.html',
-                                                           extra_context={'short_date_format': settings.SHORT_DATE_FORMAT}), name='import-step1'),
+    path('import/participants/step1', TemplateView.as_view(
+        template_name='import/step1.html',
+        extra_context={'short_date_format': settings.SHORT_DATE_FORMAT}), name='import-step1'),
     path('import/participants/step2', views.ValidateExcel.as_view(), name='import-step2'),
     path('import/participants/step3', views.ImportParticipants.as_view(), name='import-step3'),
     path('import/capture/', views.Capture.as_view(), name='capture'),
