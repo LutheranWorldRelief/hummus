bdacc043fe953348368de79c15d81c85479eea4a
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2019-10-16 11:12:40 -0600

    organizando dashboard de django jet y aplicando permiso de super user para ver acciones recientes

--- monitoring/admin_dashboard.py
+++ monitoring/admin_dashboard.py
@@ -11,34 +11,37 @@ class CustomIndexDashboard(Dashboard):
 
     def init_with_context(self, context):
         self.children.append(modules.AppList(
-            _('List Applications'),
-            exclude=('auth.*', 'microsoft_auth.*', 'jet.*', 'contenttypes.*',
-                     'sessions.*', 'sites.*', 'databse.*', 'constance.*', 'admin.*', 'database.*'),
+            _('Quick Liks'),
+            models=('monitoring.Project', 'monitoring.Organization',
+                    'monitoring.Contact', 'monitoring.Profile',
+                    'auth.User', 'auth.Group','auth.Permission',
+                    'constance.Config','monitoring.ProjectContact'),
             column=0,
             order=0
         ))
 
-        self.available_children.append(modules.LinkList)
-        self.children.append(modules.LinkList(
-            _('Supporting'),
-            children=[
-                {
-                    'title': _('Django documentation'),
-                    'url': 'http://docs.djangoproject.com/',
-                    'external': True,
-                },
-                {
-                    'title': _('Django "django-users" mailing list'),
-                    'url': 'http://groups.google.com/group/django-users',
-                    'external': True,
-                },
-            ],
-            column=0,
-            order=0
-        ))
-        self.children.append(modules.RecentActions(
-            _('Recent Actions'),
-            10,
+        self.children.append(modules.AppList(
+            _('Catalogs'),
+            models=('monitoring.Sex', 'monitoring.Country',
+                    'monitoring.OrganizationType', 'monitoring.LWRRegion',
+                    'monitoring.Product', 'monitoring.Education',
+                    'monitoring.ContactType'),
             column=1,
             order=0
         ))
+
+        self.children.append(modules.AppList(
+            _('Extras'),
+            models=('monitoring.Request', 'monitoring.Template',
+                    'monitoring.Source', 'monitoring.Filter'),
+            column=2,
+            order=0
+        ))
+
+        if context.request.user.is_superuser:
+            self.children.append(modules.RecentActions(
+                _('Recent Actions'),
+                10,
+                column=2,
+                order=1
+            ))
