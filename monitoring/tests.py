65b3fe918588b680c9b216a1867246c7635bd729
Javier Wilson <javier.wilson@gmail.com>
2019-10-24 20:15:22 +0000

    more names

--- monitoring/tests.py
+++ monitoring/tests.py
@@ -9,9 +9,7 @@ from monitoring.models import Contact
 class ContactTestCase(TestCase):
     def setUp(self):
         Contact.objects.create(first_name="Trey Polk", last_name="Polk")
-        #Contact.objects.create(first_name="Willena", last_name="Vanalstyne")
-        #Contact.objects.create(first_name="Gilberto", last_name="Gilsdorf")
-        #Contact.objects.create(first_name="Maranda", last_name="Pedro")
+        Contact.objects.create(first_name="Gilberto", last_name="Gilsdorf")
 
     def test_name_generation(self):
         """name is created from first and last name"""
