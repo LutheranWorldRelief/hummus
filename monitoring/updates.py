998e8d4060fb6a4da396493fa9fd3ef857fcb46d
stickler-ci <support@stickler-ci.com>
2019-11-01 05:51:04 +0000

    Fixing style errors.

--- monitoring/updates.py
+++ monitoring/updates.py
@@ -108,7 +108,7 @@ def validate_data(row, mapping, start_row=0, date_format=None):
                 if model._meta.get_field(field).get_internal_type() == 'DateField':
                     if not cell.is_date and not parse_date(cell.value):
                         messages.append('[{}]: {} "{}" is not a valid date.'.format(reference,
-                                                                        field, cell.value,))
+                                                                                    field, cell.value,))
 
                 # validates foreign keys
                 if model._meta.get_field(field).get_internal_type() == 'ForeignKey':
@@ -120,8 +120,8 @@ def validate_data(row, mapping, start_row=0, date_format=None):
                             options_trans = list(related_model.objects.values_list(__('name'),
                                                                                    flat=True))
                             options.extend(options_trans)
-                            messages.append('[{}]: "{}" not found in {}. Options are {}'.\
-                                format(reference, cell.value, field, options))
+                            messages.append('[{}]: "{}" not found in {}. Options are {}'.
+                                            format(reference, cell.value, field, options))
                         messages.append('[{}]: "{}" not found in {}.'.format(reference,
-                                        cell.value, field))
+                                                                             cell.value, field))
     return messages
