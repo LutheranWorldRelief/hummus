90344e16fe1c4078f656d9b712fd0c597006fd1c
stickler-ci <support@stickler-ci.com>
2019-11-17 04:26:08 +0000

    Fixing style errors.

--- hummus/settings.py
+++ hummus/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 LOCALE_PATHS = (
     os.path.join(BASE_DIR, "locale"),
 )
-FORMAT_MODULE_PATH = [ 'formats' ]
+FORMAT_MODULE_PATH = ['formats']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
