399420da34fd7cf74ea9785a626efda96af4e835
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2019-12-22 23:42:51 -0600

    reorganizacion de filtros de dashboard, agregacion de nuevas urls correspondientes a apis, reemplado de select2(jquery) x vue-multiselect y reestructuracion de variables para los inputs y parametros de envio a las api

--- monitoring/static/js/dashboard-vue.js
+++ monitoring/static/js/dashboard-vue.js
@@ -1,38 +1,86 @@
+Vue.component('vue-multiselect', window.VueMultiselect.default);
+
 var app = new Vue({
     el: '#app',
     data: {
         formInputs: {
-            proyecto: '',
+            project: {},
+            country: {},
+            lwrregion: '',
+            year: '',
+        },
+        requestParameters: {
+            project_id: '',
             paises_todos: true,
             rubros_todos: true,
-            desde: '',
-            hasta: '',
-            paises: [],
+            from_date: '',
+            to_date: '',
+            country_id: [],
+            lwrregion_id: '',
+            year: '',
         },
+        hide_project: false,
+        list_projects: [],
+        list_countries: [],
         quantity_projects: 0,
         quantity_subprojects: 0,
         quantity_participants: 0,
     },
     created() {
+        $.get(UrlsAcciones.UrlProjects)
+            .then(data => {
+                this.quantity_projects = Object.keys(data).length;
+                for (const key in data) {
+                    this.list_projects.push({
+                        name: data[key],
+                        value: key
+                    });
+                }
+            });
+
+        $.get(UrlsAcciones.UrlCountries)
+            .then(data => {
+                for (const key in data) {
+                    this.list_countries.push({
+                        name: data[key],
+                        value: key
+                    });
+                }
+            });
+
         this.loadDataForDashboard();
     },
     methods: {
         loadDataForDashboard() {
 
-            $.post(UrlsAcciones.UrlDatosCantidadProyectos, this.formInputs  )
-                .then(((response) => {
-                    this.quantity_projects = response.proyectos;
-                }));
+            if (this.formInputs.project['value']) {
+                this.hide_project = true;
+                this.requestParameters.project_id = this.formInputs.project['value'];
+            }
 
-            $.post(UrlsAcciones.UrlDatosCantidadSubproyectos, this.formInputs   )
-                .then(((response) => {
-                    this.quantity_subprojects = response.subproyectos;
-                }));
+            this.requestParameters.lwrregion_id = this.formInputs.lwrregion;
+            this.requestParameters.country_id = this.formInputs.country;
 
-            $.post(UrlsAcciones.UrlDatosCantidadParticipantes, this.formInputs  )
+            $.post(UrlsAcciones.UrlDatosCantidadParticipantes, this.requestParameters)
                 .then(((response) => {
                     this.quantity_participants = response.participantes;
                 }));
+
+            if (this.requestParameters.project_id !== '') {
+                console.log(this.requestParameters.project_id);
+                //new_url content; example = http://localhost/api/subproject/project/1/
+                let new_url = `api/subprojects/project/${this.requestParameters.project_id}/`;
+
+                $.get(new_url)
+                    .then(response => {
+                        this.quantity_subprojects = response.object_list.length;
+                    });
+            } else {
+                $.get(UrlsAcciones.UrlSubProjects)
+                    .then(response => {
+                        this.quantity_subprojects = response.object_list.length;
+                    });
+            }
         },
     }
 
