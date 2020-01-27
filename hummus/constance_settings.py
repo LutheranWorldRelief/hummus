a1ae02a53e5c41809125c8c1e6e1ffab6bc59132
Javier Wilson <javier.wilson@gmail.com>
2020-01-27 15:13:54 +0000

    fixed setttings

--- hummus/constance_settings.py
+++ hummus/constance_settings.py
@@ -4,8 +4,8 @@ settings for django constance
 CONSTANCE_CONFIG = {
     'SALESFORCE_SYNC': (True, 'Salesforce Syncronization', bool),
     'SALESFORCE_FREQ': (60, 'Salesforce Syncronization Frequency in minutes', int),
-    'HEADER_ROW': (2, 'Row', int),
-    'START_ROW': (3, 'Data starts at this row', int),
+    'HEADER_ROW': (1, 'Row', int),
+    'START_ROW': (2, 'Data starts at this row', int),
     'LOCATION_FIELD': ('location', 'Location field name', str),
     'MDC_HEADER_PREFIX': ('form.', 'Prefix for MDC headers in Excel', str),
     'DEFAULT_TEMPLATE': ('clean-template', 'Default template ID', str),
