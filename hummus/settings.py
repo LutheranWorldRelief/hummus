6d2aa424d04076e5b76e1a6339a25c522875f573
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2020-01-28 17:04:12 -0600

    separando menu de django jet de settings.py

--- hummus/settings.py
+++ hummus/settings.py
@@ -14,6 +14,7 @@ import os
 import environ
 from django.utils.translation import gettext_lazy as _
 from django.utils.log import DEFAULT_LOGGING
+from hummus.sidebar_django_jet import get_django_jet_menu
 
 # Env basic configuration
 env = environ.Env(
@@ -184,61 +185,7 @@ MICROSOFT_DOMAIN = env('MICROSOFT_DOMAIN')
 MICROSOFT_AUTH_LOGIN_TYPE = 'ma'
 
 # Jet Menu Settings
-JET_SIDE_MENU_ITEMS = [
-    {'label': _('Import Participants'), 'permissions': ['monitoring.add_projectcontact',
-                                                         'monitoring.change_projectcontact',
-                                                         'monitoring.add_contact',
-                                                         'monitoring.change_contact'], 'items': [
-        {'name': '', 'url': '/import/participants/step1  ',
-         'label': _('Import Participants')},
-    ]},
-    {'label': _('Participants'), 'permissions': ['monitoring.view_contact'], 'items': [
-        {'name': 'monitoring.contact', 'label': _('Participants'),
-         'permissions': ['monitoring.view_contact']},
-        {'name': '', 'url': '/validate/dupes-name', 'label': _('Duplicates by Name'),
-         'permissions': ['monitoring.change_contact', 'monitoring.change_projectcontact']},
-        {'name': '', 'url': '/validate/dupes-doc', 'label': _('Duplicates per document'),
-         'permissions': ['monitoring.change_contact', 'monitoring.change_projectcontact']},
-    ]},
-    {'label': _('Reports'), 'permissions': ['monitoring.view_projectcontact',
-                                            'monitoring.view_country',
-                                            'monitoring.view_contact',
-                                            'monitoring.view_organization'], 'items': [
-        {'name': '', 'url': '/export/participants', 'label': _('Project Participants')},
-        {'name': '', 'url': '/export/template-clean/', 'label': _('Clean Template'),
-         'permissions': ['monitoring.add_projectcontact',
-                         'monitoring.change_projectcontact',
-                         'monitoring.add_contact',
-                         'monitoring.change_contact']},
-    ]},
-    {'label': _('Dashboard'), 'items': [
-        {'name': '', 'url': '/dashboard/', 'label': _('Dashboard')},
-    ]},
-    {'label': _('Data Tables'), 'items': [
-        {'name': 'monitoring.project', 'label': _('Projects'), 'permissions': [
-            'monitoring.view_project']},
-        {'name': 'monitoring.organization', 'label': _('Organizations'),
-         'permissions': ['monitoring.view_organization']},
-        {'name': 'monitoring.organizationtype', 'label': _('Types'),
-         'permissions': ['monitoring.view_organizationtype']},
-        {'name': 'monitoring.country', 'label': _('Countries'), 'permissions': [
-            'monitoring.view_country']},
-        {'name': 'monitoring.contacttype', 'label': _('Contact Types'), 'permissions': [
-            'monitoring.view_contacttype']},
-        {'name': 'monitoring.education', 'label': _('Educations'), 'permissions': [
-            'monitoring.view_education']},
-        {'name': 'monitoring.sex', 'label': _('Sex'), 'permissions': ['monitoring.view_sex']},
-        {'name': 'monitoring.lwrregion', 'label': _('Regions'), 'permissions': [
-            'monitoring.view_lwrregion']},
-        {'name': 'monitoring.filter', 'label': _('Segmentation'), 'permissions': [
-            'monitoring.view_filter']},
-    ]},
-    {'label': _('Security'), 'permissions': ['request.user.is_superuser'], 'items': [
-        {'name': 'auth.user', 'label': _('Users')},
-        {'name': 'auth.group', 'label': _('Roles')},
-        {'name': 'monitoring.profile', 'label': _('Profiles')},
-    ]}
-]
+JET_SIDE_MENU_ITEMS = get_django_jet_menu()
 
 # JET dashboard custumization
 JET_INDEX_DASHBOARD = 'monitoring.admin_dashboard.CustomIndexDashboard'
